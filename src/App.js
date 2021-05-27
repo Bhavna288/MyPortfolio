8e10ce660c294603fb6a97e2a736e1cd19987d32
Bhavna288 <bhavnatahelyani288@gmail.com>
2021-05-27 20:55:11 +0530

    Header started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar"
+import Header from "./components/Header"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Header />
+        </>
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../logo.png'
 // React fontawesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
@@ -8,7 +10,7 @@ const Navbar = () => {
             <div className="container">
                 <a className="navbar-brand" href="#"><img className="logo" src={ logo } alt="logo"></img></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={ faBars } style={ { color: "#fff" } } />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -28,7 +30,6 @@ const Navbar = () => {
                             <a className="nav-link" href="#">Contact us</a>
                         </li>
                     </ul>
-
                 </div>
             </div>
         </nav>
