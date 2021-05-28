fd107ee291382a2914b69e5ff58b340945a9856e
Bhavna288 <bhavnatahelyani288@gmail.com>
2021-05-28 14:29:30 +0530

    Header and Particles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed'
 
 const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
+                <canvas></canvas>
                 <h1>Web development</h1>
+                <Typed
+                    className="typed-text"
+                    strings={ ["Web Developer", "Machine Learning", "Android Developer"] }
+                    typeSpeed={ 40 }
+                    backSpeed={ 60 }
+                    loop
+                />
+                <a href="#" className="btn-main">Contact Me</a>
             </div>
         </div>
     )
