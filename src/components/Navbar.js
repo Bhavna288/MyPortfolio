44d5340ed2afab8d4dae88227020173d130af2a5
Bhavna288 <bhavnatahelyani288@gmail.com>
2021-05-29 21:03:29 +0530

    HREF changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,20 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#"><img className="logo" src={ logo } alt="logo"></img></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={ faBars } style={ { color: "#fff" } } />
+                <a className="navbar-brand" href="/#"><img className="logo" src={ logo } alt="logo"></img></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={ faBars } style={ { color: "/#fff" } } />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
+                            <a className="nav-link" href="/#about">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#projects">Projects</a>
+                            <a className="nav-link" href="/#projects">Projects</a>
                         </li>
                     </ul>
                 </div>
